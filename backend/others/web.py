63bed20281829f61abca8b5f77b891a1fedb0f83
punkekee <irinaespasa@gmail.com>
2024-05-04 16:31:15 +0200

    Jofre Dolcet

--- backend/others/web.py
+++ backend/others/web.py
@@ -1,7 +1,1 @@
-from flask import Flask, request
-app = Flask(__name__)
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    data = request.form['input_name']
-    return str('input_name')
+print("Jofre Dolcet")
